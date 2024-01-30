ff35b5a329ec46c699365e60906f362aed7ae08e
Igor V. Sementsov <ig.kuzm@gmail.com>
2024-01-30 05:27:54 +0300

    fix

--- src/lua/redraw.lua
+++ src/lua/redraw.lua
@@ -2,7 +2,7 @@
 File              : redraw.lua
 Author            : Igor V. Sementsov <ig.kuzm@gmail.com>
 Date              : 04.01.2024
-Last Modified Date: 16.01.2024
+Last Modified Date: 30.01.2024
 Last Modified By  : Igor V. Sementsov <ig.kuzm@gmail.com>
 --]]--
 -- © 2008 David Given.
@@ -271,6 +271,16 @@ function RedrawScreen()
 			end
 
 			local x = paragraph:getIndentOfLine(ln)
+			-- get indent for CENTER
+			if paragraph.style == "CENTER" then
+				local w = 0
+				for _, wn in ipairs(lines[ln]) do
+					local word = paragraph[wn]
+					w = w + GetStringWidth(word) + 1
+				end
+				x = Document.wrapwidth / 2 - w / 2
+			end
+			
 			local l = lines[ln]
 
 			if not mp then
@@ -386,6 +396,17 @@ function RedrawScreen()
 			end
 			
 			local x = paragraph:getIndentOfLine(ln)
+			
+			-- get indent for CENTER
+			if paragraph.style == "CENTER" then
+				local w = 0
+				for _, wn in ipairs(lines[ln]) do
+					local word = paragraph[wn]
+					w = w + GetStringWidth(word) + 1
+				end
+				x = Document.wrapwidth / 2 - w / 2
+			end
+			
 			if not mp then
 				paragraph:renderLine(l,
 					leftpadding + margin + x, y)
