e05dd90b3f7ee48e01ff1f6d6148cc991d462538
Igor V. Sementsov <ig.kuzm@gmail.com>
2024-08-03 21:59:59 +1000

    on_progress

--- src/lua/menu.lua
+++ src/lua/menu.lua
@@ -2,7 +2,7 @@
 File              : menu.lua
 Author            : Igor V. Sementsov <ig.kuzm@gmail.com>
 Date              : 01.01.2024
-Last Modified Date: 15.07.2024
+Last Modified Date: 03.08.2024
 Last Modified By  : Igor V. Sementsov <ig.kuzm@gmail.com>
 --]]--
 -- © 2008 David Given.
@@ -121,6 +121,7 @@ local DocumentSettingsMenu = CreateMenu("Document settings",
 	{"FSPageCount",    "P", "Page count...",         nil,         Cmd.ConfigurePageCount},
 	{"FSSmartquotes",  "Q", "Smart quotes...",       nil,         Cmd.ConfigureSmartQuotes},
 	{"FSSpellchecker", "K", "Spellchecker...",       nil,         Cmd.ConfigureSpellchecker},
+	{"FSpageConfig",   "L", "Page config...",        nil,         Cmd.ConfigurePage},
 })
 
 local GlobalSettingsMenu = CreateMenu("Global settings",
